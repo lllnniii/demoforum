ecb30ca08e6737549f273da5b0d686a49f2ca937
lnni <163763233+lllnniii@users.noreply.github.com>
2025-04-21 12:06:18 +0300

    Delete back directory

--- back/models/categories.py
+++
@@ -1,9 +0,0 @@
-from back.database.base import Base
-from sqlalchemy import Column, Integer, String
-
-
-class Category(Base):
-    __tablename__ = 'categories'
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), index=True)
-    description = Column(String(300))
